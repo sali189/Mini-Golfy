efaf3d2675ecff1f184ffe7fbe6255ab43c15238
sali189 <sali18@luc.edu>
2017-04-18 21:48:07 -0500

    Ball stops
    Ball stops accordingly. We just have to implement onClick.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+public float launchSpeed;
+private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.velocity = new Vector3 (0,0, launchSpeed);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	/*if(Input.GetKeyDown("Fire1")){
+
+		}*/
+	}
+}
